563765d344554f5963f8495d8b88c09352c7a6c2
peterwhiffin <jordanlahaie@gmail.com>
2025-03-12 11:53:25 -0500

    first

---
+++ code/build.bat
@@ -0,0 +1,6 @@
+@echo off
+mkdir ..\..\raylibGameBuild
+pushd ..\..\raylibGameBuild
+cl gdi32.lib msvcrt.lib raylib.lib winmm.lib user32.lib shell32.lib ..\raylibGameTest\code\raylibgametest.c -IX:\Raylib\raylib-5.5_win64_msvc16\include /link /libpath:X:\Raylib\raylib-5.5_win64_msvc16\lib /NODEFAULTLIB:libcmt
+popd
+
